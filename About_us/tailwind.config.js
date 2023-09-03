f605f317887cbed7711c380c83ad4eaa5c6269ec
ThakareSakshi <sakshithakare121@gmail.com>
2023-09-03 19:46:33 +0530

    created values network and team section

--- About_us/tailwind.config.js
+++ About_us/tailwind.config.js
@@ -11,7 +11,16 @@ module.exports = {
       backgroundImage: {
         'banner-mbl': "url('https://uat-cdn.drlallab.com/2021-07/about-banner-mbl.jpg')",
         'banner-desk': "url('https://uat-cdn.drlallab.com/2022-12/about-banner-desk.png')",
+        'values-bg-img':"url(	https://uat-cdn.drlallab.com/images/bg-img2-light.jpg)",
+      },
+      backgroundColor:{
+        'div-yellow':"rgb(254,204,78)",
+        'red-bd':"rgb(255,0,0)",
+      },
+      color:{
+        'red-bd':"rgb(255,0,0)"
       }
+
     },
   },
   plugins: [],
